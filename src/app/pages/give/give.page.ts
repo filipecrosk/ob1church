8328b58d80dd53155a4d89a7eae6341c9e31cc1a
Filipe Bragança <hello@filipe.co>
2019-05-31 10:07:40 -0700

    Setup give page

--- src/app/pages/give/give.page.ts
+++ src/app/pages/give/give.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faHome,
+  faExternalLinkAlt,
+  faBan
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-give',
-  templateUrl: './give.page.html',
-  styleUrls: ['./give.page.scss'],
+  selector: "app-give",
+  templateUrl: "./give.page.html",
+  styleUrls: ["./give.page.scss"]
 })
 export class GivePage implements OnInit {
+  faHome = faHome;
+  faExternalLinkAlt = faExternalLinkAlt;
+  faBan = faBan;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
